4b6e82479b15f7b65e33adb8709098a4bc9643b4
Hildhan123 <hildhan123@gmail.com>
2023-07-27 15:14:06 +0700

    upload env

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -54,7 +54,7 @@
     <nav class="navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary sticky-top p-0">
         <a href="/" class="navbar-brand bg-primary d-flex align-items-center px-4 px-lg-5">
             <img src="{{ asset(Storage::url($desa->logo)) }}" width="50" height="50" alt="">
-            {{-- <h2 class="mb-2 text-white">Kelurahan {{ $desa->nama_kelurahan }}</h2> --}}
+            <h2 class="px-2 text-white">Kelurahan {{ $desa->nama_kelurahan }}</h2>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
